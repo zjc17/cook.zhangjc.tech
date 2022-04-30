aeda1fb63be8fc80ecf1749a421f3378a406eaeb
jiachen.zhang <zhangjc1999@gmail.com>
2022-04-30 22:34:52 +0800

    [Feature] 补充 sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+// Default code you can customize according to your requirements.
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://cook.zhangjc.tech',
+  generateRobotsTxt: true, // (optional)
+  // REST CODE READ DOCS  ...
+}
