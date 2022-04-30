4225dbfa156110c12ed479f60d45a854d8a40aac
jiachen.zhang <zhangjc1999@gmail.com>
2022-04-30 21:16:32 +0800

    [Feature] 添加GA埋点

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+
+      <GoogleAnalytics />
+
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+function GoogleAnalytics() {
+  return (
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+    </>
+  )
 }
