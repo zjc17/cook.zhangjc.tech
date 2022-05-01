597f575e1d264334e43e70a6fc96cfe723ddc732
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-01 18:02:23 +0800

    [Feature] 种植结果区域样式调整

--- pages/plant/index.tsx
+++ pages/plant/index.tsx
@@ -8,11 +8,11 @@ import plantPot from '@iconify/icons-charm/plant-pot';
 
 function PlantCard({ plantItem }: { plantItem: PlantItem }) {
   return (
-    <Grid css={{ padding: '4px 16px' }}>
+    <Grid css={{ padding: '4px 8px' }}>
       <a href={plantItem.link} target='_blank'>
         <Card>
           <Card.Body css={{ padding: '4px 8px' }}>
-            <Text h6 >
+            <Text h6 weight='normal' >
               {plantItem.name}
             </Text>
           </Card.Body>
@@ -36,7 +36,9 @@ export default function PlantComponent() {
         <Text h5 weight="normal">今天我们来种菜！</Text>
       </Row>
       <Spacer y={2} />
-      <Grid.Container xl>
+      <Grid.Container xl css={{ backgroundColor: theme.colors.accents1.value, padding: '8px' }}>
+      {/* </Grid.Container>
+      <Grid.Container xl> */}
         <Row wrap="wrap" justify="center" align="center" >
           {plants.map((value, index) => (
             <PlantCard plantItem={value} />
