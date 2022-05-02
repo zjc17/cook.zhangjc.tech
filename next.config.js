a6b8dbbbc6af46856c8881a5d20b264d2fb566a3
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-02 22:44:40 +0800

    [Bugfix] 修复export module 不正确导致pwa失效

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-const withMDX = require('@next/mdx')({
+const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
@@ -10,18 +10,17 @@ const withMDX = require('@next/mdx')({
     // If you use `MDXProvider`, uncomment the following line.
     providerImportSource: "@mdx-js/react",
   },
-})
+});
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    disable: process.env.DISABLE_PWA === true,
-  },
-})
-
-module.exports = withMDX({
-  // Append the default value with md extensions
-  reactStrictMode: true,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+module.exports = withMDX(
+  withPWA({
+    pwa: {
+      dest: "public",
+      runtimeCaching,
+      disable: process.env.DISABLE_PWA,
+    },
+    reactStrictMode: true,
+    // Append the default value with md extensions
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  })
+);
