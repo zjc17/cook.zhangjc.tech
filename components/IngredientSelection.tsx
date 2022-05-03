cd20cbe13d9904b166bad1411f36598fee60b2e6
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-03 12:59:40 +0800

    [Feature] 首页分割格式调整

--- components/IngredientSelection.tsx
+++ components/IngredientSelection.tsx
@@ -4,6 +4,7 @@ import MeatTag from "../components/tags/meat_tag"
 import StapleTag from '../components/tags/staple_tag'
 import ToolTag from '../components/tags/tool_tag'
 import { userRecipeState } from '../store/recipe'
+import { useTheme } from "@nextui-org/react";
 
 const vegatables: Array<string> = ["土豆", "胡萝卜", "花菜", "白萝卜", "西葫芦", "番茄", "芹菜", "黄瓜", "洋葱", "莴笋", "菌菇", "茄子", "豆腐", "包菜", "白菜",]
 const meats: Array<string> = ["午餐肉", "香肠", "腊肠", "鸡肉", "猪肉", "鸡蛋", "虾", "牛肉", "骨头"]
@@ -11,12 +12,12 @@ const stapleFood: Array<string> = ["面食", "面包", "米", "方便面",]
 const tools: Array<string> = ["烤箱", "空气炸锅", "微波炉", "电饭煲", "一口能炒又能煮的大锅",]
 
 export default function IngredientSelection() {
-    const toggleTool = userRecipeState(state => state.toggleTool)
+    // const toggleTool = userRecipeState(state => state.toggleTool)
     const tool: string = userRecipeState(state => state.tool);
     const toogleTool: (name: string) => void = userRecipeState(state => state.toggleTool);
     const toogleStuff: (name: string) => void = userRecipeState(state => state.toggleStuff);
     const stuffs: Set<string> = userRecipeState(state => state.stuffs);
-
+    const {theme} = useTheme()
 
     return <Container>
         <Grid.Container xl >
@@ -34,7 +35,7 @@ export default function IngredientSelection() {
                 {vegatables.map((value, index) =>
                     <VegetableTag id={index} key={index} value={value} isSelected={stuffs.has(value)} toogleTag={toogleStuff} />)}
             </Row>
-            <br/>
+            <Spacer style={{height: theme.space[1].value}} />
             <Row justify="center" align="center">
                 <Text h4 weight="bold" css={{ opacity: 0.8 }}>
                     🥩 肉肉们
@@ -44,7 +45,7 @@ export default function IngredientSelection() {
                 {meats.map((value, index) =>
                     <MeatTag id={index} key={index} value={value} isSelected={stuffs.has(value)} toogleTag={toogleStuff} />)}
             </Row>
-            <br/>
+            <Spacer style={{height: theme.space[1].value}} />
             <Row justify="center" align="center">
                 <Text h4 weight="bold" css={{ opacity: 0.8 }}>
                     🍚 来点主食？（不选也行）
@@ -54,8 +55,7 @@ export default function IngredientSelection() {
                 {stapleFood.map((value, index) =>
                     <StapleTag id={index} key={index} value={value} isSelected={stuffs.has(value)} toogleTag={toogleStuff} />)}
             </Row>
-
-            <br/><br/>
+            <Spacer style={{height: theme.space[2].value}} />
             <Row justify="center" align="center">
                 <Text h3 weight="bold" css={{ opacity: 0.8 }}>
                     🍳 再选一下厨具
