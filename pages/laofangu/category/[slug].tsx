2612ab1e6fa224c2155503de2dd3be9f5faf319a
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-03 14:36:08 +0800

    [Feature] 老饭骨合集及内容

---
+++ pages/laofangu/category/[slug].tsx
@@ -0,0 +1,82 @@
+import { Text } from "@nextui-org/react"
+import { LaofanguCategory, LaofanguCategoryItem } from "../../../components/types/laofangu";
+import { LaofanguDish, LaofanguDishItem } from "../../../components/types/laofangu";
+import AboutLayout from "../../../components/layouts/AboutLayout";
+import { Row, Spacer, Grid, Card } from "@nextui-org/react";
+import { useTheme } from "@nextui-org/react";
+
+export default function Category({ category, dishes }: StaticProps) {
+  const { theme } = useTheme()
+  return (
+    <>
+      <AboutLayout showBilibiliFooter={false}>
+        <Row justify="center" align="center">
+          <Text h3 weight="medium">老饭骨美食合集-{category.name}</Text>
+        </Row>
+        <Spacer style={{ height: theme.space[2].value }} />
+        <Grid.Container xl css={{ backgroundColor: theme.colors.accents1.value, padding: '8px' }}>
+          <Row wrap="wrap" justify="center" align="center" >
+            {dishes.map((value, index) => (
+              <TraditionalCard id={index} key={index} LaofanguDishItem={value} />
+            ))}
+          </Row>
+        </Grid.Container>
+      </AboutLayout>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  // import categoryJsonData from '../../data/laofangu_category.json'
+  // import { LaofanguCategory, LaofanguCategoryItem } from "../../components/types/laofangu";
+  const categoryJsonData = (await import('../../../data/laofangu_category.json')).default
+  const data = categoryJsonData as LaofanguCategory
+  // Get the paths we want to pre-render based on posts
+  const paths = data.map((category) => ({
+    params: {
+      slug: category.slug,
+      data: category,
+    },
+  }));
+  // Set fallback to blocking. Now any new post added post build will SSR
+  // to ensure SEO. It will then be static for all subsequent requests
+  return { paths, fallback: 'blocking' };
+}
+
+export async function getStaticProps({ params }) {
+  const categoryJsonData = (await import('../../../data/laofangu_category.json')).default
+  const categoryData = categoryJsonData as LaofanguCategory
+  const category = categoryData.find(category => category.slug === params.slug)
+
+  const laofanguJsonData = (await import('../../../data/laofangu.json')).default
+  const laofanguData = laofanguJsonData as LaofanguDish
+  const dishes: LaofanguDish = laofanguData.filter(dish => dish.category.split(',').includes(category.name))
+  const props: StaticProps = {
+    category,
+    dishes,
+  }
+  return {
+    props: props,
+  };
+}
+
+interface StaticProps {
+  category: LaofanguCategoryItem
+  dishes: LaofanguDish
+}
+
+function TraditionalCard({ id: key, LaofanguDishItem }: { id: string | number, LaofanguDishItem: LaofanguDishItem }) {
+  return (
+    <Grid key={key} style={{ padding: '4px 8px' }}>
+      <a href={LaofanguDishItem.link} target='_blank'>
+        <Card clickable>
+          <Card.Body style={{ padding: '4px 8px' }}>
+            <Text h6 weight='normal' >
+              {LaofanguDishItem.name}
+            </Text>
+          </Card.Body>
+        </Card>
+      </a>
+    </Grid>
+  )
+}
--- pages/laofangu/index.tsx
+++ pages/laofangu/index.tsx
@@ -7,50 +7,7 @@ import { Icon } from '@iconify/react';
 import plantPot from '@iconify/icons-charm/plant-pot';
 import categoryJsonData from '../../data/laofangu_category.json'
 import { LaofanguCategory, LaofanguCategoryItem } from "../../components/types/laofangu";
-
-function TraditionalCard({ LaofanguDishItem }: { LaofanguDishItem: LaofanguDishItem }) {
-  return (
-    <Grid key={LaofanguDishItem.name} style={{ padding: '4px 8px' }}>
-      <a href={LaofanguDishItem.link} target='_blank'>
-        <Card>
-          <Card.Body style={{ padding: '4px 8px' }}>
-            <Text h6 weight='normal' >
-              {LaofanguDishItem.name}
-            </Text>
-          </Card.Body>
-        </Card>
-      </a>
-    </Grid>
-  )
-}
-
-function CategoryCard({ categoryItem }: { categoryItem: LaofanguCategoryItem }) {
-  const { theme, type } = useTheme()
-  return (
-    <Grid key={categoryItem.slug} style={{ padding: '4px 8px' }} css={{ width: '25%', p: 0, minWidth: 280, aspectRatio: 1.5 }}>
-      <Card cover >
-        <Card.Body >
-          <Card.Image
-            src={categoryItem.cover}
-            height={200}
-            width="100%"
-            alt="Relaxing app background"
-          />
-        </Card.Body>
-        <Card.Footer blur css={{
-          position: "absolute",
-          bgBlur: theme.colors.accents1.value,
-          bottom: 0,
-          zIndex: 1,
-        }}>
-          <Text h5 weight='semibold' >
-            {categoryItem.name}
-          </Text>
-        </Card.Footer>
-      </Card>
-    </Grid>
-  )
-}
+import Link from "next/link";
 
 export default function Categories() {
   const [data, setData] = useState(categoryJsonData)
@@ -58,16 +15,11 @@ export default function Categories() {
   const { theme, type } = useTheme()
   return (
     <AboutLayout maxWidth="1200px" showBilibiliFooter={false}>
-      {/* <Row justify="center" align="center" >
-        <Icon icon={plantPot} width="44" height="44" color={type === 'light' ? theme.colors.green600.value : theme.colors.green400.value} />
-      </Row> */}
       <Row justify="center" align="center">
         <Text h3 weight="medium">老饭骨美食合集</Text>
       </Row>
       <Spacer y={2} />
       <Grid.Container xl css={{ backgroundColor: theme.colors.accents1.value, padding: '8px' }}>
-        {/* </Grid.Container>
-      <Grid.Container xl> */}
         <Row wrap="wrap" justify="center" align="center" >
           {laofanguCategory.map((value, index) => (
             <CategoryCard categoryItem={value} />
@@ -78,23 +30,32 @@ export default function Categories() {
   )
 }
 
-function Traditional() {
-  const [data, setData] = useState(jsonData);
-  const laofanguDishes = data as LaofanguDish
+function CategoryCard({ categoryItem }: { categoryItem: LaofanguCategoryItem }) {
   const { theme, type } = useTheme()
   return (
-    <AboutLayout showBilibiliFooter={false}>
-      <Row justify="center" align="center">
-        <Text h5 weight="normal">老饭骨美食合集</Text>
-      </Row>
-      <Spacer y={2} />
-      <Grid.Container xl css={{ backgroundColor: theme.colors.accents1.value, padding: '8px' }}>
-        <Row wrap="wrap" justify="center" align="center" >
-          {laofanguDishes.map((value, index) => (
-            <TraditionalCard LaofanguDishItem={value} />
-          ))}
-        </Row>
-      </Grid.Container>
-    </AboutLayout>
+    <Grid key={categoryItem.slug} style={{ padding: '4px 8px' }} css={{ width: '25%', p: 0, minWidth: 280, aspectRatio: 1.5 }}>
+      <Link href={`/laofangu/category/${categoryItem.slug}`}>
+        <Card cover clickable >
+          <Card.Body >
+            <Card.Image
+              src={categoryItem.cover}
+              height={200}
+              width="100%"
+              alt="Relaxing app background"
+            />
+          </Card.Body>
+          <Card.Footer blur css={{
+            position: "absolute",
+            bgBlur: theme.colors.accents1.value,
+            bottom: 0,
+            zIndex: 1,
+          }}>
+            <Text h5 weight='semibold' >
+              {categoryItem.name}
+            </Text>
+          </Card.Footer>
+        </Card>
+      </Link>
+    </Grid>
   )
 }
