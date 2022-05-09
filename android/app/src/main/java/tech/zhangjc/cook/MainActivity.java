898c78491962705654835776c1b54647e90a95fb
jiachen.zhang <zhangjc1999@gmail.com>
2022-05-09 23:05:09 +0800

    [Feature] build android with capacitor

---
+++ android/app/src/main/java/tech/zhangjc/cook/MainActivity.java
@@ -0,0 +1,5 @@
+package tech.zhangjc.cook;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tech.zhangjc.cook',
+  appName: '居家食用手册',
+  bundledWebRuntime: false,
+  webDir: 'out'
+};
+
+export default config;
