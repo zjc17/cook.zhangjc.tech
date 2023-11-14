7919f9217fea63d5a9f60658a88238ca05a67f8d
Jiachen <zhangjc1999@gmail.com>
2023-11-15 01:59:41 +0800

    [Feature] use gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,13 +32,47 @@ export default function App({ Component, pageProps }: AppProps) {
       }}
     >
       <NextUIProvider>
-        <GoogleAnalytics />
+        {/*<GoogleAnalytics />*/}
+        <GTMScript gtmId={'GTM-KSC9667D'}/>
         {process.env.NEXT_PUBLIC_ENABLE_WEB_VITAL_SCRIPT && <WebVitalsScript />}
         <BaiduTrace />
         {/* <GoogleAdsTrace /> */}
         <Component {...pageProps} />
       </NextUIProvider>
     </NextThemesProvider>
+  )
+}
+
+const GTMScript = ({gtmId}: { gtmId?: string }) => {
+  if (!gtmId) {
+    return null
+  }
+  return (
+    <>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              />`,
+        }}
+      />
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${gtmId}');`
+        }}
+      />
+    </>
+
   )
 }
 
